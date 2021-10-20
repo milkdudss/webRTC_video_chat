c3d50e40568e642a696a37ca367395f6b34c4d27
Albert Koshy <albertkoshy@alberts-air.lan>
2021-10-20 16:22:53 -0700

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import { ContextProvider } from './Context';
+// import { ContextProvider } from './Context';
 
 import './styles.css';
 
-ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
